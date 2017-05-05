30d63255b8fe6e15cac48cb00b3be2f1718c9b04
Raphael Alex Silva Abreu <raphaelabreu@mec.gov.br>
2017-05-05 17:06:59 -0300

    navigation-map reviewing and fixes. (need improvements for lazy loading implementation for use with google maps libraries)

---
+++ src/assets/js/gmaps.js
@@ -0,0 +1,65 @@
+(function() {})
+
+// http://stackoverflow.com/questions/32496382/typeerror-window-initmap-is-not-a-function
+// http://stackoverflow.com/questions/40448238/initmap-is-not-a-function
+// http://stackoverflow.com/questions/41065766/initmap-is-not-a-function-google-maps-javascript
+// http://stackoverflow.com/questions/34466718/googlemaps-does-not-load-on-page-load
+// https://forum.ionicframework.com/t/error-initmap-is-not-a-function-google-maps/62532
+
+var map = new google.maps.Map(document.getElementById('navigation-map'), {
+  center: {lat: -34.397, lng: 150.644},
+  zoom: 6
+});
+
+
+var infoWindow = new google.maps.InfoWindow({map: map});
+
+function initMap() {
+  // Enabling new cartography and themes
+  google.maps.visualRefresh = true;
+
+  // Setting starting options
+  var mapOptions = {
+    center: new google.maps.LatLng(39.9078, 32.8252),
+    zoom: 10,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  // Getting Map DOM Element
+  var mapElement = document.getElementById('gisMap');
+  // Creating a map with DOM element
+  map = new google.maps.Map(mapElement, mapOptions);
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+}
+
+
+// Try HTML5 geolocation.
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var pos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    };
+
+    console.log(pos.lat);
+
+    console.log(pos.lng);
+
+    infoWindow.setPosition(pos);
+    infoWindow.setContent('Location found.');
+    map.setCenter(pos);
+
+  }, function() {
+    handleLocationError(true, infoWindow, map.getCenter());
+  });
+} else {
+
+  // Browser doesn't support Geolocation
+  handleLocationError(false, infoWindow, map.getCenter());
+}
--- src/components/navigation-map/navigation-map.component.ts
+++ src/components/navigation-map/navigation-map.component.ts
@@ -1,7 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { NavigationMapService } from './navigation-map.service';
-
-const url = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAZ9yVeau18-qAqqxAidwA_M8ZW1IEgPXQ&signed_in=true&callback=initMap';
 
 @Component({
   selector: 'app-navigation-map',
@@ -11,30 +9,16 @@ const url = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAZ9yVeau18-qAqqxA
 // http://stackoverflow.com/questions/34542143/load-external-css-style-into-angular-2-component
 // http://stackoverflow.com/questions/42138068/how-to-add-and-use-custom-js-file-in-angular-2-app-in-typescript-components
 // http://stackoverflow.com/questions/34489916/load-external-js-script-dynamically-in-angular-2
-export class NavigationMapComponent {
+export class NavigationMapComponent implements AfterViewInit {
   private navigationMapService: NavigationMapService;
-  loadAPI: Promise<any>;
 
   constructor(navigationMapService: NavigationMapService) {
     this.navigationMapService = navigationMapService;
     this.initializeComponent();
   }
 
-  private initializeComponent(): void {
-    this.loadAPI = new Promise((resolve) => {
-      console.log('resolving promise...');
-      this.loadAPIScript();
-    });
-  }
+  private initializeComponent(): void {  }
 
-  public loadAPIScript() {
-    console.log('preparing to load...')
-    let node = document.createElement('script');
-    node.src = url;
-    node.type = 'text/javascript';
-    node.async = true;
-    node.charset = 'utf-8';
-    //document.getElementsByTagName('head')[0].appendChild(node);
-  }
+  ngAfterViewInit(): void {  }
 
 }
