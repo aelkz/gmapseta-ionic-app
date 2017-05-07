13a48d304aeb5c13dc79f8d72f84f08bc56baa1f
aelkz <raphael.alex@gmail.com>
2017-05-07 14:54:02 -0300

    best-route component fixes to allow re-rendering when component is destroyed. Need to add google maps api scripts into header section, to force object instances created at page loads.

--- src/assets/js/gmaps.js
+++ src/assets/js/gmaps.js
@@ -6,10 +6,20 @@
 
 // https://developers.google.com/maps/documentation/javascript/infowindows?hl=pt-br
 
+var myExtObject = (function() {
+  console.log('myExtObject called.');
+  return {
+    func1: function() {
+      console.log('func1 called.');
+      loadMap();
+    }
+  }
+})(myExtObject||{})
+
 document.onreadystatechange = function () {
   if (document.readyState == "complete") {
     // document is ready. Do your stuff here
-    loadMap();
+    //loadMap();
   }
 }
 
--- src/components/navigation-map/navigation-map.component.ts
+++ src/components/navigation-map/navigation-map.component.ts
@@ -1,5 +1,7 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 import { NavigationMapService } from './navigation-map.service';
+
+declare var myExtObject: any;
 
 @Component({
   selector: 'app-navigation-map',
@@ -9,7 +11,7 @@ import { NavigationMapService } from './navigation-map.service';
 // http://stackoverflow.com/questions/34542143/load-external-css-style-into-angular-2-component
 // http://stackoverflow.com/questions/42138068/how-to-add-and-use-custom-js-file-in-angular-2-app-in-typescript-components
 // http://stackoverflow.com/questions/34489916/load-external-js-script-dynamically-in-angular-2
-export class NavigationMapComponent implements AfterViewInit {
+export class NavigationMapComponent implements AfterViewInit, OnInit {
   private navigationMapService: NavigationMapService;
 
   constructor(navigationMapService: NavigationMapService) {
@@ -17,8 +19,18 @@ export class NavigationMapComponent implements AfterViewInit {
     this.initializeComponent();
   }
 
-  private initializeComponent(): void {  }
+  private initializeComponent(): void { }
 
-  ngAfterViewInit(): void {  }
+  CallFunction1() {
+    console.log('CallFunction1 called.');
+    myExtObject.func1();
+  }
+
+  ngOnInit(){
+    this.CallFunction1();
+  }
+
+  ngAfterViewInit(): void { 
+  }
 
 }
